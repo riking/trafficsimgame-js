5f9b64980c3d935240139bf591e0e01ff388e5ec
kanepyork@gmail.com <kane@KANEUBUNTU.(none)>
2011-11-19 22:32:59 -0800

    Added variables to Car to prepare for rendering.

--- src/Car.java
+++ src/Car.java
@@ -5,7 +5,9 @@ public class Car
 	int destination;
 	Route route;
 	int current;
-
+	float renderx;
+	float rendery;
+	double yaw;
 	public void overrideRoute(int next) //can ONLY call from traffic lights
 	{
 		route.overrideRoute(current,next,destination);
