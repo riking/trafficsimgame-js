9e71f31827d57a9bcad563241633d009b0935270
riking <riking28@gmail.com>
2011-12-07 13:21:56 -0800

    new algorithm. do not compile; not done writing.

--- src/RoutingThread.java
+++ src/RoutingThread.java
@@ -4,27 +4,66 @@ import java.util.*;
 
 public class RoutingThread extends Thread
 {
-	Node pointA;
-	Node pointB;
-	LinkedList<Node> fromStart;
-	LinkedList<Node> fromEnd;
-	BareRoute arg;
-	static int maxiter=30;
+	Node cur, con, start, end;
+	Node[] links;
+	double[] distarray;
+	byte[] visitstatus;
+	short stopcount;
+	boolean dontstop;
+	ListIterator liter;
+	static short extraIters = 5;
 	public RoutingThread(BareRoute br)
 	{
-		arg=br;
-		pointB=Node.nodelist.get(br.routeStart);
-		pointA=Node.nodelist.get(br.routeEnd);
-		fromStart=new LinkedList<Node>();
-		fromEnd=new LinkedList<Node>();
-		fromStart.add(pointA);
-		fromEnd.add(pointB);
+		links = new Node[Node.maxid];
+		distarray = new double[Node.maxid];
+		visitstatus = new byte[Node.maxid];
+		start=Node.getId(br.routeStart);
+		end=Node.getId(br.routeEnd);
 	}
 	public void run()
 	{
-		
-	}
-	public void check()
-	{
+		/** Visit Status array
+		 * 0 = unknown
+		 * 1 = queued
+		 * 2 = finished
+		 */
+		stopcount = 0;
+		visitstatus[start.id] = 1;
+		while(stopcount < extraIters)
+		{
+			dontstop = false;
+			for(int i = 1; i < Node.maxid; i++) // Start at 1 because node 0 is NPE (by design)
+			{
+				if(visitstatus[i] == 1)
+				{
+					cur = Node.getId(i);
+					liter = cur.connections.listIterator();
+					while(liter.hasNext())
+					{
+						con = liter.next();
+						if(visitstatus[con.id] == 0)
+						{
+							visitstatus[con.id] = 1;
+							distarray[con.id] = distarray[i] + cur.getDist(con);
+							links[con.id] = cur;
+						}
+						else if (distarray[con.id] > distarray[i] + cur.getDist(con))
+						{
+							distarray[con.id] = distarray[i] + cur.getDist(con);
+							links[con.id] = cur;
+							if(visitstatus[con.id] == 2)
+								dontstop = true;
+							visitstatus[con.id] = 1;
+						}
+					}
+					visitstatus[i] = 2;
+				}
+			}
+			if(visitstatus[end.id] == 2 && !dontstop)
+			{
+				stopcount++;
+			}
+		}
+		//build route
 	}
 }
