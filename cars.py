4f54cea184a4b5d16752fad06b6659169064120c
Riking <rikingcoding@gmail.com>
2012-04-06 13:37:48 -0700

    PEP8: 4 spaces instead of tabs

--- cars.py
+++ cars.py
@@ -1,140 +1,140 @@
 import world
 
 class Car:
-	def __init__(self,origin,map,rand,destination=None):
-		if destination==None:
-			destination = rand.choice(map.nodelist)
-		self.startnode = origin
-		self.lastnode = origin
-		self.curnode = None # node we're traveling towards
-		self.curroad = None # road we're on
-		self.nextnode = None # node _after_ the curnode
-		self.route = []
-		self.routedirection = 1 # so route caching will work, if we want to go thru the route backwards
-		self.destnode = destination
-		self.timetonext = 1
+    def __init__(self,origin,map,rand,destination=None):
+        if destination==None:
+            destination = rand.choice(map.nodelist)
+        self.startnode = origin
+        self.lastnode = origin
+        self.curnode = None # node we're traveling towards
+        self.curroad = None # road we're on
+        self.nextnode = None # node _after_ the curnode
+        self.route = []
+        self.routedirection = 1 # so route caching will work, if we want to go thru the route backwards
+        self.destnode = destination
+        self.timetonext = 1
 
-	def __str__(self):
-		return "car at %s with %d ticks to %s" % (self.curroad.getNode(self.curnode),self.timetonext,self.curnode)
+    def __str__(self):
+        return "car at %s with %d ticks to %s" % (self.curroad.getNode(self.curnode),self.timetonext,self.curnode)
 
-	def __del__(self):
-		#print("deleting car")
-		pass
+    def __del__(self):
+        #print("deleting car")
+        pass
 
-	def destination(self):
-		return self.destnode
+    def destination(self):
+        return self.destnode
 
-	def getTime(self,road):
-		return int(road.dist()/30)
-		
-	def getPosition(self):
-		t=self.getTime(self.curroad)
-		return self.lastnode.pos + (self.curnode.pos - self.lastnode.pos)*(t-self.timetonext)/t
-		
-	def routeInit(self,origin,map,rand):
-		if origin==self.destnode:
-			return
-		r = doRoute(self,origin,self.destnode,map)
-		if r ==None:
-			return False
-		self.route = r
-		self.curroad = origin.connections[r[1]]
-		return True
+    def getTime(self,road):
+        return int(road.dist()/30)
+        
+    def getPosition(self):
+        t=self.getTime(self.curroad)
+        return self.lastnode.pos + (self.curnode.pos - self.lastnode.pos)*(t-self.timetonext)/t
+        
+    def routeInit(self,origin,map,rand):
+        if origin==self.destnode:
+            return
+        r = doRoute(self,origin,self.destnode,map)
+        if r ==None:
+            return False
+        self.route = r
+        self.curroad = origin.connections[r[1]]
+        return True
 
-	def notifyRoadChange(self,turnnode,newnode,oldroad,newroad,map):
-		self.lastnode = turnnode
-		self.curnode = newnode
-		self.curroad = newroad
-		self.timetonext = self.getTime(newroad)
-		i = 0
-		try:
-			i = self.route.index(newnode)
-		except ValueError:
-			self.route = doRouteT(self,turnnode,newnode,self.destnode,map)
-			i = self.route.index(newnode)
-		try:
-			if self.route[i+self.routedirection] == turnnode:
-				self.routedirection *= -1
-		except IndexError:
-			print("Car giving up!")
-			self.destnode = newnode
-			self.nextnode = newnode
-			return
-		else:
-			self.nextnode = self.route[i+self.routedirection]
+    def notifyRoadChange(self,turnnode,newnode,oldroad,newroad,map):
+        self.lastnode = turnnode
+        self.curnode = newnode
+        self.curroad = newroad
+        self.timetonext = self.getTime(newroad)
+        i = 0
+        try:
+            i = self.route.index(newnode)
+        except ValueError:
+            self.route = doRouteT(self,turnnode,newnode,self.destnode,map)
+            i = self.route.index(newnode)
+        try:
+            if self.route[i+self.routedirection] == turnnode:
+                self.routedirection *= -1
+        except IndexError:
+            print("Car giving up!")
+            self.destnode = newnode
+            self.nextnode = newnode
+            return
+        else:
+            self.nextnode = self.route[i+self.routedirection]
 
-		
-	
-	def getNextNode(self,oldroad,turnnode,map):
-		return self.nextnode
-		
-	
-	def tick(self,road,node,map,rand):
-		self.onTick(road,node,map,rand)
-		self.timetonext -= 1
-		return self.timetonext <= 0
-			
-	def emergency_reroute(self,road,turnnode,map):
-		r = doRoute(self,turnnode,self.destnode,map)
-		self.route = r
-		try:
-			return r[1]
-		except Exception as exc:
-			self.cleanup() #delete self
-	
-	def onTick(self,road,node,map,rand):
-		pass
-	
-	def cleanup(self):
-		pass #i think that cars don't make double refs, they just need to be del'd
-		#del self.route
-		#del self.startnode
-		#del self.destnode
-	
+        
+    
+    def getNextNode(self,oldroad,turnnode,map):
+        return self.nextnode
+        
+    
+    def tick(self,road,node,map,rand):
+        self.onTick(road,node,map,rand)
+        self.timetonext -= 1
+        return self.timetonext <= 0
+            
+    def emergency_reroute(self,road,turnnode,map):
+        r = doRoute(self,turnnode,self.destnode,map)
+        self.route = r
+        try:
+            return r[1]
+        except Exception as exc:
+            self.cleanup() #delete self
+    
+    def onTick(self,road,node,map,rand):
+        pass
+    
+    def cleanup(self):
+        pass #i think that cars don't make double refs, they just need to be del'd
+        #del self.route
+        #del self.startnode
+        #del self.destnode
+    
 baseCarType = Car
 
 routecache = {}
 
 def clearroutecache():
-	routecache = {}
+    routecache = {}
 
 
 # Better routing algo (using dist):
 # .py import operator;sorted({"three":3, "five":5, "four":4, "one":1}.values())
 def doRoute(car,begin,end,map):
-	if (begin, end) in routecache:
-		return routecache[(begin,end)]
-	if (end,begin) in routecache:
-		return routecache[(end,begin)][::-1]
-	#key = node, val = nodefrom
-	bestroute = {begin:begin}
-	queue=[begin]
-	
-	try:
-		while end not in bestroute:
-			nod = queue.pop()
-			for n in nod.connections.keys():
-				if n not in bestroute:
-					bestroute[n]=nod
-					queue.append(n)
-				if n == end:
-					break
-	except IndexError as exc:
-		return None
-	finally:
-		del queue
-	#start building route
-	n = end
-	a=[n]
-	while n != begin:
-		a.append(bestroute[n])
-		n=bestroute[n]
-	return a[::-1]
-			
-	
+    if (begin, end) in routecache:
+        return routecache[(begin,end)]
+    if (end,begin) in routecache:
+        return routecache[(end,begin)][::-1]
+    #key = node, val = nodefrom
+    bestroute = {begin:begin}
+    queue=[begin]
+    
+    try:
+        while end not in bestroute:
+            nod = queue.pop()
+            for n in nod.connections.keys():
+                if n not in bestroute:
+                    bestroute[n]=nod
+                    queue.append(n)
+                if n == end:
+                    break
+    except IndexError as exc:
+        return None
+    finally:
+        del queue
+    #start building route
+    n = end
+    a=[n]
+    while n != begin:
+        a.append(bestroute[n])
+        n=bestroute[n]
+    return a[::-1]
+            
+    
 def doRouteT(car,node1,node2,end,map):
-	a = doRoute(node1,node2,map).remove(node2)
-	b = doRoute(node2,end,map)
-	return a+b
-	
-	
+    a = doRoute(node1,node2,map).remove(node2)
+    b = doRoute(node2,end,map)
+    return a+b
+    
+    
--- world.py
+++ world.py
@@ -9,109 +9,113 @@ import cars
 import node
 
 class Map:
-	def __init__(self,siz):
-		self.size = siz
-		self.nodelist = []
-	
-	def __del__(self):
-		for n in self.nodelist:
-			n.cleanup()
-		
-	def makeNodes(self,density,roadrange,rand):
-		for i in range(int(self.size * density)):
-			q=None
-			#should replace with a customizable weights function
-			if rand.random() < 0.1:
-				q=node.CarGenNode(((rand.randint(1,self.size), rand.randint(1,self.size))))
-			else:
-				q=node.Node((rand.randint(1,self.size), rand.randint(1,self.size)))
-				
-			for n in self.nodelist:
-				if n.dist(q) < roadrange *2* density:
-					break
-			else:
-				self.nodelist.append(q)
-		return self
-	
-	def makeRoads(self,roadsize,roadchance,passes,rand):
-		for i in range(passes):
-			for n in self.nodelist:
-				try:
-					a=[]
-					for m in self.nodelist: #get each node in range
-						if n.dist(m) < roadsize:
-							a.append(m)
-					m = rand.choice(a)
-					a.remove(m)
-					n.addConnection(m)
-					while rand.random() < roadchance: #maybe connect to them
-						m = rand.choice(a)
-						a.remove(m)
-						n.addConnection(m)
-					
-				except IndexError:
-					# if there's no nodes within roadrange, find the nearest other node
-					
-					t=None
-					a=self.size**2 #distance to closest node
-					for m in self.nodelist:
-						d = n.dist(m)
-						if d < a:
-							a = d
-							t = m
-					if t==None:
-						#hmmmmmmmmm........ does this mean that there is no nodes?
-						print("attempted to add roads to node %s, no node in range." % n.coordstr())
-						continue
-					else:
-						pass #print("made long-road from %s to %s" % (n.coordstr(),t.coordstr()))
-		return self
-	
-	def print_(self):
-		output_size = 40
-		output_res = self.size / output_size
-		#
-		FILLER = ' '
-		ROAD = '+'
-		NODE = '#'
-		CAR = '>'
-		#
-		import array;
-		output = []
-		for i in range(output_size+1):
-			output.append(array.array('u',[FILLER for j in range(output_size+1)]))
-		
-		print("drawing roads")
-		for n in self.nodelist:
-			for r in n.connections.values():
-				m = r.getNode(n)
-				diff = (n.pos - m.pos)/20
-				for i in range(20):
-					tem = (n.pos - diff*i)//output_res
-					output[tem[0]][tem[1]] = ROAD
-		
-		print("drawing cars")
-		for n in self.nodelist:
-			for r in n.connections.values():
-				for c in r.carList(n):
-					p=c.getPosition()//output_res
-					output[p[0]][p[1]] = CAR
-		
-		print("drawing nodes")
-		for n in self.nodelist:
-			p = n.pos// output_res
-			output[p[0]][p[1]] = NODE
-		
-		
-		# print output[]
-		for i in range(output_size):
-			print(''.join([j for j in output[i]]))
-			
+    def __init__(self,siz):
+        self.size = siz
+        self.nodelist = []
+    
+    def __del__(self):
+        for n in self.nodelist:
+            n.cleanup()
+        
+    def makeNodes(self,density,roadrange,rand):
+        for i in range(int(self.size * density)):
+            q = None
+            #should replace with a customizable weights function
+            if rand.random() < 0.1:
+                q = node.CarGenNode(
+                    (rand.randint(1,self.size), 
+                     rand.randint(1, self.size))
+                    )
+            else:
+                q = node.Node(
+                    (rand.randint(1, self.size), rand.randint(1, self.size)))
+                
+            for n in self.nodelist:
+                if n.dist(q) < roadrange *2* density:
+                    break
+            else:
+                self.nodelist.append(q)
+        return self
+    
+    def makeRoads(self,roadsize,roadchance,passes,rand):
+        for i in range(passes):
+            for n in self.nodelist:
+                try:
+                    a=[]
+                    for m in self.nodelist: #get each node in range
+                        if n.dist(m) < roadsize:
+                            a.append(m)
+                    m = rand.choice(a)
+                    a.remove(m)
+                    n.addConnection(m)
+                    while rand.random() < roadchance: #maybe connect to them
+                        m = rand.choice(a)
+                        a.remove(m)
+                        n.addConnection(m)
+                    
+                except IndexError:
+                    # if there's no nodes within roadrange, find the nearest other node
+                    
+                    t=None
+                    a=self.size**2 #distance to closest node
+                    for m in self.nodelist:
+                        d = n.dist(m)
+                        if d < a:
+                            a = d
+                            t = m
+                    if t==None:
+                        #hmmmmmmmmm........ does this mean that there is no nodes?
+                        print("attempted to add roads to node %s, no node in range." % n.coordstr())
+                        continue
+                    else:
+                        pass #print("made long-road from %s to %s" % (n.coordstr(),t.coordstr()))
+        return self
+    
+    def print_(self):
+        output_size = 40
+        output_res = self.size / output_size
+        #
+        FILLER = ' '
+        ROAD = '+'
+        NODE = '#'
+        CAR = '>'
+        #
+        import array;
+        output = []
+        for i in range(output_size+1):
+            output.append(array.array('u',[FILLER for j in range(output_size+1)]))
+        
+        print("drawing roads")
+        for n in self.nodelist:
+            for r in n.connections.values():
+                m = r.getNode(n)
+                diff = (n.pos - m.pos)/20
+                for i in range(20):
+                    tem = (n.pos - diff*i)//output_res
+                    output[tem[0]][tem[1]] = ROAD
+        
+        print("drawing cars")
+        for n in self.nodelist:
+            for r in n.connections.values():
+                for c in r.carList(n):
+                    p=c.getPosition()//output_res
+                    output[p[0]][p[1]] = CAR
+        
+        print("drawing nodes")
+        for n in self.nodelist:
+            p = n.pos// output_res
+            output[p[0]][p[1]] = NODE
+        
+        
+        # print output[]
+        for i in range(output_size):
+            print(''.join([j for j in output[i]]))
+            
 
-	def tick(self,rand):
-		for n in self.nodelist:
-			n.tick(self,rand)
-	
+    def tick(self,rand):
+        for n in self.nodelist:
+            n.tick(self,rand)
+    
 ###############################################################################
 ###############################################################################
 ###############################################################################
@@ -119,53 +123,53 @@ class Map:
 ###############################################################################
 
 class Road:
-	def __init__(self,n1,n2):
-		self.node1 = n1
-		self.node2 = n2
-		self.distance = math.sqrt((n1.pos[0] - n2.pos[0])**2 + (n1.pos[1] - n2.pos[1])**2)
-		self.cars = {n1:[],n2:[]}
+    def __init__(self,n1,n2):
+        self.node1 = n1
+        self.node2 = n2
+        self.distance = math.sqrt((n1.pos[0] - n2.pos[0])**2 + (n1.pos[1] - n2.pos[1])**2)
+        self.cars = {n1:[],n2:[]}
 
-	def getNode(self,refnode=None):
-		return self.node2 if refnode==self.node1 else self.node1
-		
-	def carList(self,refnode=None):
-		if not refnode:
-			return self.cars
-		else:
-			return self.cars[refnode]
-			
-	def dist(self):
-		return self.distance	
+    def getNode(self,refnode=None):
+        return self.node2 if refnode==self.node1 else self.node1
+        
+    def carList(self,refnode=None):
+        if not refnode:
+            return self.cars
+        else:
+            return self.cars[refnode]
+            
+    def dist(self):
+        return self.distance    
 
-	def addCarFrom(self,onode,car):
-		self.cars[self.getNode(onode)].append(car)
-	
-	def tick(self,onode,map,rand):
-		self.onTick(onode,map)
-		for car in self.cars[onode]:
-			if car.tick(self,onode,map,rand):
-				onode.carArrived(car,self,map)
-				self.cars[onode].remove(car)
-		
-	def onTick(self,onode,map):
-		pass #reserved for subclasses
-		
-	def cleanup(self): #on map exit. clean up circular references
-		try:
-			for a in self.cars:
-				for c in a:
-					try:
-						c.cleanup()
-					except (NameError, AttribuiteError):
-						pass #dump it. this is because we're cleaning up circular references
-			del self.cars
-		except (NameError, AttribuiteError):
-			pass #whee, circular references are fun!
-		try:
-			del self.node1
-			del self.node2
-		except (NameError, AttribuiteError):
-			pass
+    def addCarFrom(self,onode,car):
+        self.cars[self.getNode(onode)].append(car)
+    
+    def tick(self,onode,map,rand):
+        self.onTick(onode,map)
+        for car in self.cars[onode]:
+            if car.tick(self,onode,map,rand):
+                onode.carArrived(car,self,map)
+                self.cars[onode].remove(car)
+        
+    def onTick(self,onode,map):
+        pass #reserved for subclasses
+        
+    def cleanup(self): #on map exit. clean up circular references
+        try:
+            for a in self.cars:
+                for c in a:
+                    try:
+                        c.cleanup()
+                    except (NameError, AttribuiteError):
+                        pass #dump it. this is because we're cleaning up circular references
+            del self.cars
+        except (NameError, AttribuiteError):
+            pass #whee, circular references are fun!
+        try:
+            del self.node1
+            del self.node2
+        except (NameError, AttribuiteError):
+            pass
 
 ###############################################################################
 ###############################################################################
@@ -183,18 +187,18 @@ class Road:
 ###############################################################################
 
 def mapGen(seed=None,size=2000,density=.3,roadrange=300,roadchance=.4,roadpasses=2):
-	r = random.Random(seed)
-	m = Map(size).makeNodes(density,roadrange,r).makeRoads(roadrange,roadchance,roadpasses,r)
-	return m,r
-	
+    r = random.Random(seed)
+    m = Map(size).makeNodes(density,roadrange,r).makeRoads(roadrange,roadchance,roadpasses,r)
+    return m,r
+    
 if __name__ == "__main__":
-	m,r = mapGen()
-	m.print_()
-	import time
-	for i in range(1000):
-		m.tick(r)
-		m.print_()
-		time.sleep(0.5)
+    m,r = mapGen()
+    m.print_()
+    import time
+    for i in range(1000):
+        m.tick(r)
+        m.print_()
+        time.sleep(0.5)
 
 
-	
+    
