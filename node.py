1dc4f75b2dfc43cec25fbfdcba48888aff993098
Riking <rikingcoding@gmail.com>
2012-04-06 13:50:04 -0700

    PEP8: node.py

--- node.py
+++ node.py
@@ -6,8 +6,8 @@ import world
 import cars
 
 class Node:
-    #self.connections = {node: road, node: road}
-    def __init__(self,position,connect=None,manager=None):
+    # self.connections = {node: road, node: road}
+    def __init__(self, position, connect=None, manager=None):
         self.pos = vectors.Vector(position)
         self.connections = {}
         if connect:
@@ -19,17 +19,17 @@ class Node:
             self.imanager = ZeroWaitIntersection(self)
                 
     
-    def __eq__(self,other):
-        if isinstance(other,Node):
+    def __eq__(self, other):
+        if isinstance(other, Node):
             return self.pos == other.pos
-        if isinstance(other,vectors.Vector):
+        if isinstance(other, vectors.Vector):
             return self.pos == other
         return NotImplemented
         
-    def __ne__(self,other):
-        if isinstance(other,Node):
+    def __ne__(self, other):
+        if isinstance(other, Node):
             return self.pos != other.pos
-        if isinstance(other,vectors.Vector):
+        if isinstance(other, vectors.Vector):
             return self.pos != other
         return NotImplemented
         
@@ -40,45 +40,45 @@ class Node:
         return str(self.pos)
         
     def __repr__(self):
-        return "Node(%s,%s)"%(str(self.pos),len(self.connections))
+        return "Node(%s,%s)" % (str(self.pos), len(self.connections))
         
     def getPos(self):
         return self.pos
     
-    def addConnection(self,other):
-        r = world.Road(self,other)
+    def addConnection(self, other):
+        r = world.Road(self, other)
         if other in self.connections.keys():
             return
         self.connections[other] = r
-        other.addConnectionCB(self,r)
+        other.addConnectionCB(self, r)
         
-    def addConnectionCB(self,other,road):
+    def addConnectionCB(self, other, road):
         self.connections[other] = road
         
-    def getRoad(self,other):
+    def getRoad(self, other):
         if other in self.connections:
             return self.connections[other]
         return None
     
-    def tick(self,map,rand):
-        self.onTick(map,rand)
+    def tick(self, map, rand):
+        self.onTick(map, rand)
         for r in self.connections.values():
-            r.tick(self,map,rand)
+            r.tick(self, map, rand)
     
-    def onTick(self,map,rand):
-        pass #Reserved for subclasses
+    def onTick(self, map, rand):
+        pass   # Reserved for subclasses
         
-    def carArrived(self,car,road,map):
+    def carArrived(self, car, road, map):
         if self == car.destination():
             print("car arrived")
             print(car)
             car.cleanup()
             del car
-            return #hopefully this will kill it
-        self.imanager.carArrived(self,car,road,map)
+            return   # hopefully this will kill the car
+        self.imanager.carArrived(self, car, road, map)
     
     def dist(self,other):
-        return math.sqrt((self.pos[0] - other.pos[0])**2 + (self.pos[1] - other.pos[1])**2)
+        return math.sqrt((self.pos[0] - other.pos[0]) ** 2 + (self.pos[1] - other.pos[1]) ** 2)
 
     def coordstr(self):
         return str(self.pos)
@@ -87,20 +87,21 @@ class Node:
         cstr = ""
         for c,r in self.connections.items():
             cstr += c.coordstr()
-        return "Node at %s \nConnected to: %s" % (self.pos,cstr)
+        return "Node at %s \nConnected to: %s" % (self.pos, cstr)
         
-    def cleanup(self): #on map exit. clean up circular references
+    def cleanup(self):  # on map exit. clean up circular references
         try:
-            for n,r in self.connections.items():
+            for n, r in self.connections.items():
                 r.cleanup()
             del self.connections
-        except NameError as exc:
-            pass #...then dump it
+        except (NameError, AttributeError) as exc:
+            pass  # dump all exceptions
+
 
 
 class CarGenNode (Node):
-    def __init__(self,position,cartype = None, delay=20):
-        Node.__init__(self,position)
+    def __init__(self, position, cartype = None, delay=20):
+        Node.__init__(self, position)
         self.delay = delay
         if not cartype:
             cartype = cars.baseCarType
@@ -108,21 +109,21 @@ class CarGenNode (Node):
         self.tickcount = 1
         
     
-    def onTick(self,map,rand):
+    def onTick(self, map, rand):
         self.tickcount -= 1
         if self.tickcount <= 0:
             self.tickcount = self.delay
             dest = rand.choice(map.nodelist)
             if dest == self:
                 return
-            c = self.car(self,map,rand,)
-            if not c.routeInit(self,map,rand):
+            c = self.car(self, map, rand, dest)
+            if not c.routeInit(self, map, rand):
                 del c
                 return
-            self.carArrived(c,None,map)
+            self.carArrived(c, None, map)
 
-#Lane Data
-# dict ( Road, *1* )
+# Lane Data format
+# lanedata = dict ( Road, *1* )
 # len(lanedata) = connection number
 # *1* = list [*2*]
 #len(*1*) = number of lanes
@@ -144,42 +145,42 @@ class CarGenNode (Node):
 
 
 class Intersection:
-    def __init__(self,node,lanedata = None):
+    def __init__(self, node, lanedata = None):
         self.parent = node
         if lanedata:
-            pass
+            pass  # TODO: Unpack lane data
         
-    def tick(self,node,map,rand):
-        self.onTick(node,map,rand)
+    def tick(self, node, map, rand):
+        self.onTick(node, map, rand)
         
-    def onTick(self,node,map,rand):
-        pass
+    def onTick(self, node, map, rand):
+        pass  # Reserved for subclasses
     
-    def laneSort(self,car,road,map):
+    def laneSort(self, car, road, map):
         raise NotImplementedError()
     
-    def carArrived(self,parent,car,road,map):
+    def carArrived(self, parent, car, road, map):
         raise NotImplementedError()
     
-    def passCar(self,node,car,roadfrom,map):
-        nn = car.getNextNode(roadfrom,node,map)
+    def passCar(self, parent, car, roadfrom, map):
+        nn = car.getNextNode(roadfrom, parent, map)
         if nn not in node.connections.keys():
-            nn = car.emergency_reroute(roadfrom,node,map)
+            nn = car.emergency_reroute(roadfrom, parent, map)
             if nn not in node.connections.keys():
                 #print("car.emergency_rereoute failed to give a valid next node, deleting car")
                 car.cleanup()
                 del car
                 return
-        node.connections[nn].addCarFrom(self,car)
-        car.notifyRoadChange(node,nn,roadfrom,node.connections[nn],map)
+        node.connections[nn].addCarFrom(self, car)
+        car.notifyRoadChange(parent, nn, roadfrom, parent.connections[nn], map)
     
     
 class ZeroWaitIntersection (Intersection):
-    def __init__(self,node):
-        Intersection.__init__(self,node)
+    def __init__(self, node):
+        Intersection.__init__(self, node)
     
-    def carArrived(self,parent,car,road,map):
-        self.passCar(parent,car,road,map)
+    def carArrived(self, parent, car, road, map):
+        self.passCar(parent, car, road, map)
         
     def laneSort(self,car,road,map):
         return 0
